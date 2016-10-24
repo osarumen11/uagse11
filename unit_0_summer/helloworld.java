8ed16642fdddc4b90855360f5d2e8c055b0d72db
osarumenosagieuag <osarumenosagie@uagateway.org>
2016-10-24 18:09:19 +0000

    initial commit

---
+++ unit_0_summer/helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Welcome to APCSA!");
+        System.out.println("I cant believe im taking another year of Software Engineering!! Hope I dont fail!!");
+    }
+}
